46957fff03a84f2d9dad5dacacd9ef5bcc81b91b
sallysface <eva.gilbert@ucas-edu.net>
2025-08-29 14:42:34 -0600

    wrote note

---
+++ Notes/varibles.py
@@ -0,0 +1,20 @@
+# EG 7th varibles notes
+#What is one way to have your program give an output?
+    #print statment 
+print("Welcome to your pain and suffering!")
+#What is a variable?
+    #they hold peices of information
+#How do I assign a variable?
+    # varible_name = varible_value
+school = "UCAS"
+single_digit = 4
+print (single_digit)
+#How do I reassign a variable?
+    #just change whats inside the varible (varible_name = Varible_value2)
+single_digit = 6
+print (single_digit)
+#How do I create an input into my program?
+    # Varible_name = input("ask a question")
+fav_num = input('What is your favorite number')
+#How many things can be put into the same print statement?
+    #wouldn't you like to know weatherboy
