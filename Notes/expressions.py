e86115b819aa1557d3a7070b2beb8de96847a105
sallysface <eva.gilbert@ucas-edu.net>
2025-09-04 14:40:48 -0600

    Notes

---
+++ Notes/expressions.py
@@ -0,0 +1,40 @@
+#EG 7th Expressions notes
+
+#What is an algorithm?
+     #A set of instructions to complete a task
+name = input("What is your name?\n")
+print ("ello", name)
+#Treyson has 12 apples, he has 5 friends he wants to give apples to, hgoow many apples does each friend get
+apples = 12
+friend = 5
+print ("each friend gets", apples/friend, "apples!")
+
+
+
+#List steps in an algorithm
+    #1. declare varibles (get the info you need)
+    #2. perform any equations (do the things)
+    #3. print results
+    #Average age in a group of four friends
+age1 = 4
+age2 = 6
+age3 = 2
+age4 = 36
+average=age1 + age2 + age3 + age4
+total = average/4
+print ("the average age of this group is",total )
+#List all of the different mathmatical operations
+num1=12
+num2=7
+
+print ("addition(+):", num1+num2)
+print ("subtraction(-):", num1-num2)
+print ("Multiplication(*):", num1*num2)
+print ("Division(/):", num1/num2)
+print ("Exponents(**):", num1**num2)
+print ("Integer division(//):", num1//num2)
+print ("Modulo(%):", num1%num2)
+#integer division only gives you the whole integer
+
+#List all of the diferent assignment operators
+#Why are expressions so important in coding?
--- Practices/first_program.py
+++ Practices/first_program.py
@@ -1,2 +1,5 @@
 #EG 7th first program
 
+user = input('What is your name?')
+color = input ("what is your favorite color?")
+print ("hi", user, color)
