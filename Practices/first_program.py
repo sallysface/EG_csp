70245eecac18742f0d4af9fbce276460eeb29a3d
sallysface <eva.gilbert@ucas-edu.net>
2025-09-05 19:48:32 +0000

    program fixed

--- Practices/first_program.py
+++ Practices/first_program.py
@@ -1,5 +1,6 @@
 #EG 7th first program
 
 user = input('What is your name?')
+print ("hi", user,)
 color = input ("what is your favorite color?")
-print ("hi", user, color)
+print ("Wow, I love the color", color)
