b7eac802069f9b80d6581128637959c5a4404584
sallysface <eva.gilbert@ucas-edu.net>
2025-09-08 14:41:31 -0600

    notes

--- Notes/strings.py
+++ Notes/strings.py
@@ -1,2 +1,21 @@
 #EG 7th strings notes
-print ("hahha")
+
+
+#What makes something a string?
+    #letters or numbers held together with quotation marks
+name = input("What yo name?: \n").strip().title()
+sentence = 'the quick brown fox jumps over the lazy brown dog'
+freak = name + " " + sentence
+print ("welcom", freak)
+#Why do we have strings?
+
+#How do stupid proofing and sanitization relate to each other?
+#What is debugging?
+#How do you debug the different types of errors?
+#Describe what each of the methods below does:
+#find()
+#concatenate (add)
+    # puts the strings together
+#upper()
+#lower()
+#strip()                    
