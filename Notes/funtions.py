50e62f68734fd340be1ccb05b457f3a97988409a
sallysface <eva.gilbert@ucas-edu.net>
2025-09-15 14:37:57 -0600

    notes

---
+++ Notes/funtions.py
@@ -0,0 +1,29 @@
+#EG 7th functions notes
+#print and input are most common functions
+
+ #What a function is
+    #code that only runs when called
+#Why we use functions
+    #smaller code
+#How to write a function in Python
+def welcome():
+    name = input("what is your name?")
+    print(f"Hello, {name}")
+    welcome()
+  
+print("ts is not in my function 💀🥀")
+def add(number):
+    number += num2
+    print(number)
+
+num1 = 12
+num2 = 5
+add(80,8)
+
+
+#What parameters and arguments are
+#parameters are whats in the parantheses in the function and an aurgument is the one that is 
+#How to use parameters and arguments in python
+#What return statements are
+# how you leave the function
+#How to use return statements in a program
