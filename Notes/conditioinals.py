9271bc39d2997e4da70094e3caffb73daa972987
sallysface <eva.gilbert@ucas-edu.net>
2025-09-18 14:21:32 -0600

    notes were partaken in

--- Notes/conditioinals.py
+++ Notes/conditioinals.py
@@ -1,7 +1,7 @@
 #EG 7th Conditionals
 
 #What puts something inside of the “if” statement?
- # 
+ # tab
 homework_done = input("is your homework done?").strip().capitalize()
 
 if homework_done == "No":
@@ -31,5 +31,16 @@ else:
 #== Question "Are they Equal?"
 #! not equal to
 #What are the 3 logical operators?
+ # and, or, not
+name = input("what is your name?")
+
+if name == "Ms. LaRose":
+    print('You are the teacher!')
+elif name == "Tia" or name == 'Ashley':
+    print('You are a TA!')
+else:
+    print(f"Hello {name}, you are a student")
 #What are logical operators for?
+ #to clarify
 #What does a nested conditional statement do?
+ #only runs when the statment it's in is true
