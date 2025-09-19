4522f5e83c493a6106c7660921f842ba6257154a
sallysface <eva.gilbert@ucas-edu.net>
2025-09-19 18:46:12 +0000

    coded

--- Practices/old_enough.py
+++ Practices/old_enough.py
@@ -4,3 +4,4 @@ age = int(input("How old are you?"))
 
 if age >= 4 and age < 15
     print("You are old enough to go to school!")
+if age 
--- Practices/silly_sentence.py
+++ Practices/silly_sentence.py
@@ -3,4 +3,5 @@
 
 adj= input("Give me an adjective: \n").lower().strip()
 med_crime = input("Give me a crime that involes the medical field: \n").lower().strip()
-print("House is a", adj, " tale about a dotor who often commits", med_crime, ". As well as showing his work life")
+active = input ("Give me  fun activty \n").lower().strip()
+print("House is a", adj, " tale about a dotor who often commits", med_crime, ". As well as showing his home life. House likes to", active, ".")
