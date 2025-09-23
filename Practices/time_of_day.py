f5066b0519117cddfb6f9cb9a1869c4b5c27f5bb
sallysface <eva.gilbert@ucas-edu.net>
2025-09-23 19:22:53 +0000

    time

--- Practices/time_of_day.py
+++ Practices/time_of_day.py
@@ -1,9 +1,19 @@
 #EG 7th time of day
 
 import time
+import datetime
 
 epoch = time.time()
 read_time = time.ctime(epoch)
 
-if read_time >= 7 and read_time < 11:
-   print(f'Good morning! it is {read_time}')
+now = datetime.datetime.now()
+hour = now.hour
+
+if hour >= 5 and hour < 11:
+   print(f'Good morning! it is {hour}')
+if hour >= 12 and hour < 18:
+   print(f'Good afternoon! it is {hour}')
+if hour >= 19 and hour < 22:
+   print(f'Good evening! it is {hour}')
+if hour >= 23 and hour < 4:
+   print(f'It is {hour}, you should probably be in bed right now...')
