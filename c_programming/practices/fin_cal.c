aec2b1a840b37b80836dd91a7f41eefc2995ccee
sallysface <eva.gilbert@ucas-edu.net>
2025-10-08 14:41:28 -0600

    coded

---
+++ c_programming/practices/fin_cal.c
@@ -0,0 +1,14 @@
+//EG 7th financial calculator 
+#include <stdio.h>
+int main(void){
+    float income ;
+
+    printf("what is your monthly income:\n");
+    scanf("%f", &income);
+
+
+
+
+
+    return 0;
+}
