aa5365ca556cc2fb20649322b865b59121c5b3b3
sallysface <eva.gilbert@ucas-edu.net>
2025-10-10 17:44:38 +0000

    I HATE IT HERE

--- c_programming/practices/name_decorater.c
+++ c_programming/practices/name_decorater.c
@@ -1,16 +1,18 @@
 //EG 7th name decorater
 #include <stdio.h>
-#include<string.h>
+#include <string.h>
 int main(void){
     char user_name[20];
     char back_deco[] = " ☆ ⋆★ ";
     char front_deco[] = " ★ ⋆☆ ";
 
     printf("What is your first name? \n");
-    scanf("%c", user_name);
+    scanf("%s", user_name);
+
+
     strcat(front_deco, user_name);
     strcat(user_name, back_deco);
-    printf("Your super cool name is \n %c", user_name);
+    printf("Your super cool name is:\n%s\n", user_name);
 
-
+    return 0;
 }
