dddb26e9ce06eb961e9eb05d7c2605ee26b6bad9
sallysface <eva.gilbert@ucas-edu.net>
2025-10-13 19:20:52 +0000

    doned

--- c_programming/practices/name_decorater.c
+++ c_programming/practices/name_decorater.c
@@ -5,14 +5,17 @@ int main(void){
     char user_name[20];
     char back_deco[] = " ☆ ⋆★ ";
     char front_deco[] = " ★ ⋆☆ ";
+    char decorated_name[60];
 
     printf("What is your first name? \n");
-    scanf("%s", user_name);
+    scanf("%19s", user_name); // limit input to avoid overflow
 
+    // Build decorated name: front + name + back
+    strcpy(decorated_name, front_deco);
+    strcat(decorated_name, user_name);
+    strcat(decorated_name, back_deco);
 
-    strcat(front_deco, user_name);
-    strcat(user_name, back_deco);
-    printf("Your super cool name is:\n%s\n", user_name);
+    printf("Your super cool name is:\n%s\n", decorated_name);
 
     return 0;
 }
