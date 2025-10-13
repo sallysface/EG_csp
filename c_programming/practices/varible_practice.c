5c2223008f0d73ca16761240d775b47662987a4a
sallysface <eva.gilbert@ucas-edu.net>
2025-10-13 19:42:47 +0000

    practice

---
+++ c_programming/practices/varible_practice.c
@@ -0,0 +1,15 @@
+//EG 7th varible practice
+#include <stdio.h>
+int main(void){
+    char name[] = "Eva";
+    int num = 7;
+    float gpa = 3.14;
+    char brekky[] = "None";
+    char color[] = "purple";
+    char school[] = "UCAS";
+    int year = 2025;
+    char eye[] = "Hazel";
+    int age = 14;
+    char subject[] = "Biology";
+
+}
