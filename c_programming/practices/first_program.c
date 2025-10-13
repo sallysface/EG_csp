3810d22b81e9dc9d56a287ccdbf2a089f8ee1d5f
sallysface <eva.gilbert@ucas-edu.net>
2025-10-13 19:33:21 +0000

    yippe

---
+++ c_programming/practices/first_program.c
@@ -0,0 +1,17 @@
+//EG 7th first program
+#include <stdio.h>
+int main(void){
+    char name[20];
+
+    printf("What is your first name? \n");
+    scanf("%s", name);
+
+    printf("Hello, %s, welcome to your first c program! \n", name);
+
+
+
+
+
+
+
+}
