8bef47d0a85b7430f59d45f5c75cd30defef18f4
sallysface <eva.gilbert@ucas-edu.net>
2025-10-14 14:41:40 -0600

    meow

---
+++ c_programming/notes/functions.c
@@ -0,0 +1,39 @@
+//EG 7th functions notes
+#include <stdio.h>
+void birthday(char* name, int age){
+    printf("Happy Birthday to you. \n");
+    printf("Happy Birthday to you. \n");
+    printf("Happy Birthday to you. \n");
+    printf("Happy Birthday to you. \n");
+    printf("Happy Birthday %s. You are %d. \n", name, age);
+}
+
+int main(void){
+    birthday("Jesus", 15);   
+    birthday("Sam", 16);
+    birthday("Gwen", 16);
+
+    return 0;
+}
+
+
+
+
+//What a function is
+    //a peice of code that can be reused over 
+//Why we use functions
+    //to save space and time
+//How to write a function in C
+    // void name(parameter){things}
+//What are arguments and parameters?
+    // parameters are setting function, arguments are calling functions
+//How do arguments and parameters work together?
+    //
+//How to use parameters and arguments in C
+    //
+//What are return statements?
+    //
+//How do return statements change how you define a function in C?
+    //
+//What do return statements do with the information?
+    //
